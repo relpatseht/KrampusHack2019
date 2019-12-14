c89db573deae2edfbc8b93edc9d6b8fe459a11e3
Andrew Shurney <relpatseht@gmail.com>
2019-12-14 13:00:06 -0800

    Setup base build system

---
+++ code/source/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "allegro5/allegro.h"
+
+int main(int argc, char* argv[])
+{
+	if (!al_init())
+	{
+		printf("Failed to initialize allegro.\n");
+		return -1;
+	}
+	else
+	{
+		ALLEGRO_DISPLAY* display;
+	}
+
+	return 0;
+}
